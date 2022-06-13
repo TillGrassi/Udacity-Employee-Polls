e7f730bac5821256aa4b48fdf7503a7b5697297e
TillGrassi <97838495+TillGrassi@users.noreply.github.com>
2022-06-13 09:54:40 +0200

    Add files via upload

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { _getUsers, _getQuestions } from "../_DATA";
+import { receiveQuestions } from "./questions";
+import { receiveUsers } from "./users";
+import { setAuthedUser } from "./authedUser";
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return Promise.all([_getUsers(), _getQuestions()]).then(
+      ({ users, questions }) => {
+        dispatch(receiveUsers(users));
+        dispatch(receiveQuestions(questions));
+        dispatch(setAuthedUser());
+      }
+    );
+  };
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
